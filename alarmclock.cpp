bb3a345b0338bd1f2f0c926ac602743c6dfa8f54
Mehmet Süzer <93345336+mehmetSuzer@users.noreply.github.com>
2022-07-21 13:48:21 +0300

    Update alarmclock.cpp

--- alarmclock.cpp
+++ alarmclock.cpp
@@ -1,28 +1,5 @@
 
 
-/*
---------------- ALARM CLOCK PROJECT FOR RASPBERRY PI PICO ----------------
-
-SSD1306 128x64 OLED display is used as screen
-It uses builtin RTC to handle time operations
-
-While Core 0 displays texts on the screen,
-Core 1 handles mainly logical operations and user inputs
-
-Alarm is disabled by default
-Multiple alarms cannot be set
-Alarm keeps firing at most 60 seconds
-Setting alarm automatically enables it
-When the current mode is CLOCK, it activate SLEEP MODE after a while
-In SLEEP MODE, the display is blank. To awake the machine, press a button
-The communication between cores is maintained by global variables
-Builtin LED indicates that Pico gets power and starts both cores
-Instead of sleep, busy_wait is used because of the interrupt of alarm
-
---------------------------------------------------------------------------
-*/
-
-
 #include <stdio.h>
 #include "pico/stdlib.h"
 #include "pico/multicore.h"
